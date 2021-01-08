56905dcd5d6de6f28811698aab8dd1b57e2439d0
unknown <jsuegr@gmail.com>
2021-01-08 14:51:09 -0600

    8. Primer controlador User Controller

---
+++ src/main/java/net/purocodigo/backendcrusojava/controllers/UserController.java
@@ -0,0 +1,10 @@
+package net.purocodigo.backendcrusojava.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users") // localhost:8080/users
+public class UserController {
+    
+}
---
+++ src/test/java/net/purocodigo/backendcrusojava/BackendcursojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.purocodigo.backendcrusojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendcursojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
