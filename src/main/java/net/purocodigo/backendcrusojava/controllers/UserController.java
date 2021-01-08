ee115419159dbf29d3ddfbb741fedef3effbb0f4
unknown <jsuegr@gmail.com>
2021-01-08 16:41:36 -0600

    13. conexión a la base de datos

--- src/main/java/net/purocodigo/backendcrusojava/controllers/UserController.java
+++ src/main/java/net/purocodigo/backendcrusojava/controllers/UserController.java
@@ -1,5 +1,7 @@
 package net.purocodigo.backendcrusojava.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users") // localhost:8080/users
 public class UserController {
     
+
+    @GetMapping
+    public String getUser(){
+        return "get user details";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create new user";
+    }
 }
